88bfd469e14351274259b83264dc2bc002fb4611
Javad Nasrollahi <javadnasrollahi68@gmail.com>
2023-07-09 02:32:23 +0330

    init

---
+++ android/app/src/main/java/com/example/nav/flutter_animated_bottom_navigation_bar/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.nav.flutter_animated_bottom_navigation_bar;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_bottom_navigation_bar/models/tabIcon.dart';
+import 'package:flutter_animated_bottom_navigation_bar/widgets/bottom_bar_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Animated Bottom Navigation Bar'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+
+  AnimationController? animationController;
+
+  List<TabIconData> tabIconsList = TabIconData.tabIconsList;
+  Widget tabBody = Container(
+    color:Colors.red,
+  );
+  @override
+  void initState() {
+    tabIconsList.forEach((TabIconData tab) {
+      tab.isSelected = false;
+    });
+    tabIconsList[0].isSelected = true;
+
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFFF2F3F8),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: FutureBuilder<bool>(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox();
+            } else {
+              return Stack(
+                children: <Widget>[
+                  tabBody,
+                  bottomBar(),
+                ],
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    return true;
+  }
+
+  Widget bottomBar() {
+    return Column(
+      children: <Widget>[
+        const Expanded(
+          child: SizedBox(),
+        ),
+        BottomBarView(
+          tabIconsList: tabIconsList,
+          addClick: () {
+            setState(() {
+              tabBody = Container(
+                color:Colors.transparent,
+              );
+            });
+          },
+          changeIndex: (int index) {
+              animationController?.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  if(index == 0){
+                    tabBody = Container(
+                      color:Colors.red,
+                    );
+                  }else if(index == 1){
+                    tabBody = Container(
+                      color:Colors.green,
+                    );
+                  }else if(index == 2){
+                    tabBody = Container(
+                      color:Colors.yellow,
+                    );
+                  }else if(index == 3){
+                    tabBody = Container(
+                      color:Colors.orange,
+                    );
+                  }
+                });
+              });
+            }
+        ),
+      ],
+    );
+  }
+}
